0373ad8eebedf6697afb5aa2d472b9f929252806
skyler-yang <skyler.w.yang@newegg.com>
2025-03-21 15:04:55 +0800

    update install command

--- front/projects/web/src/views/administration/nodes/common.ts
+++ front/projects/web/src/views/administration/nodes/common.ts
@@ -48,7 +48,12 @@ export function NewCommand(ip: string, isUninstall?: boolean) {
   return `docker run -d --name=humpback-agent \\
 --net=host \\
 --restart=always \\
--e ip=${ip} \\
+--privileged \\
 -v /etc/localtime:/etc/localtime \\
-humpback:latest`
+-v /var/run/docker.sock:/var/run/docker.sock \\
+-v /var/lib/docker:/var/lib/docker \\
+-e HUMPBACK_AGENT_API_BIND=${ip}:8018 \\
+-e HUMPBACK_SERVER_HOST={server-address}:{server-backend-port} \\
+-e HUMPBACK_VOLUMES_ROOT_DIRECTORY=/var/lib/docker \\
+docker.io/humpbacks/humpback-agent:develop`
 }
